512d840017e2fe5a3969ae357f4370eba3b156c6
scankin <scott.ankin@devopsgroup.com>
2021-12-15 10:54:43 +0000

    Function to get the top 5 processes running on machine added

--- powershell-scripts/highcpu.ps1
+++ powershell-scripts/highcpu.ps1
@@ -1,5 +1,6 @@
 
 Function CheckHigh-CPU {  
+    #Returns the number of processes running on the machine
     Function Get-NumProcesses {
         try {
             $ProcessCount = (Get-Process).Count
@@ -11,7 +12,32 @@ Function CheckHigh-CPU {
         }
     }
 
+    #Function to get the Top 5 Processes running on the machine
     Function Get-Processes {
+        try {
 
+            [Array]$TopFiveProcesses = @()
+
+            $Processes = Get-Process | 
+                         Sort-Object CPU -desc |
+                         Select -first 5 |
+                         Select ProcessName,ID,CPU,Description
+
+            ForEach ($Process in $Processes){
+                $Output = [PSCustomObject]@{
+                    ID = $Process.Id
+                    ProcessName = $Process.ProcessName
+                    CPU = [math]::round($Process.CPU, 2)
+                    Description = $Process.Description
+                }
+
+                $TopFiveProcesses += $Output
+            }
+            return $TopFiveProcesses
+        }
+        catch {
+            $TopFiveProcesses = "Error Occured"
+            return $TopFiveProcess
+        }
     }
 }
