6affa04fd8c345894a5e6627072d19a2175f531e
scankin <scott.ankin@devopsgroup.com>
2022-01-06 10:35:53 +0000

    runbook

--- Azure/terraform-code/high-cpu-ss/aa.tf
+++ Azure/terraform-code/high-cpu-ss/aa.tf
@@ -7,3 +7,16 @@ resource "azurerm_automation_account" "aa" {
 
   tags = var.tags
 }
+
+resource "azurerm_automation_runbook" "highCPU" {
+  name = "HighCPU-Runbook"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.aa.name
+  log_verbose = "true"
+  log_progess = "true"
+  description = "This runbook creates a log file of information surrounding the CPU usage."
+  runbook_type = "PowerShell"
+
+  
+}
