90a72fbc838a52d61ada6c9aa65bd1da101e4027
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-04 15:56:08 +0000

    Terraform version change in core-network to 2.0

--- Azure/terraform-code/high-cpu-ss/core-network/main.tf
+++ Azure/terraform-code/high-cpu-ss/core-network/main.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = "~>2.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.69"
+      version = "~>2.0"
     }
   }
 }
