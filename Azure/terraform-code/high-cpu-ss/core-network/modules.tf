d033a76a7f2a4ef28035a10c3244dc2d40d2b01c
Scott Ankin <90317732+scankin@users.noreply.github.com>
2022-02-15 16:51:35 +0000

    Development (#14)
    * Changes to automation and monitor
    
    * Tags added, scale set sku updated, Powershell modules updated to have a better console output
    
    * Tags updated
    
    * Bug fix, convert LastBootup to String before output
    
    * Changes to corenet and Monitor
    
    * Adding GitHub Actions
    
    * Adding GitHub Actions bug fix
    
    * Adding GitHub Actions bug fix
    
    * Terraform version change in core-network to 2.0
    
    * Terraform version change in core-network to 2.0
    
    * Terraform version change in core-network to 2.0
    
    * Adding GitHub Actions bug fix
    
    * Adding GitHub Actions bug fix
    
    * Added action to core-network
    
    * Added action to core-network
    
    * Changing path
    
    * Changing path
    
    * Changing path
    
    * Adding reusable yaml
    
    * Adding reusable yaml
    
    * Adding reusable yaml
    
    * Changing path
    
    * Changing path
    
    * Changing path
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Update azure-core-network.yml
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Changing deployment method
    
    * Changing deployment method
    
    * adding workflow_dispatch to github workflows
    
    * Scankin patch 1 (#12)
    
    * Development (#11)
    
    * Changes to automation and monitor
    
    * Tags added, scale set sku updated, Powershell modules updated to have a better console output
    
    * Tags updated
    
    * Bug fix, convert LastBootup to String before output
    
    * Changes to corenet and Monitor
    
    * Adding GitHub Actions
    
    * Adding GitHub Actions bug fix
    
    * Adding GitHub Actions bug fix
    
    * Terraform version change in core-network to 2.0
    
    * Terraform version change in core-network to 2.0
    
    * Terraform version change in core-network to 2.0
    
    * Adding GitHub Actions bug fix
    
    * Adding GitHub Actions bug fix
    
    * Create manual.yml
    
    * adding workflow_dispatch to github workflows
    
    * Removing Manual.yml
    
    * Removing Manual.yml
    
    * Removing Manual.yml
    
    * Changing path to reusable pipeline
    
    * Changing Azure-Core-Network.yml to reuse azure-deployment.yml
    
    * adding workflow_call trigger to azure-deployment.yml
    
    * removing workflow_dispatch from azure-core-network.yml
    
    * adding type to inputs
    
    * Changing to upper case variable names
    
    * Changing to upper case variable names
    
    * Changing variables
    
    * Adding DSC Script to install Gremlin on Scale Set VM
    
    * Adding DSC Script to install Gremlin on Scale Set VM
    
    * Adding DSC zip file
    
    * adding DSC zip
    
    * Removing zip file
    
    * Chnaging powershell file for install gremlin
    
    * Automatically install gremlin on scale set vms, scale set rules updated, action groups added
    
    * Changes to gremlin install ps1 file adn monitor.tf
    
    * Changing gremlin install script
    
    * Modifying YAML Github Actions
    
    * Adding variable check job
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Modifying YAML Github Actions
    
    * Fixing merge conflicts
    
    * Editing YAML file
    
    * Editing YAML file
    
    * Editing YAML file
    
    * Editing YAML file
    
    * removing modules call
    
    * removing unneeded yaml

--- Azure/terraform-code/high-cpu-ss/core-network/modules.tf
+++ Azure/terraform-code/high-cpu-ss/core-network/modules.tf
@@ -7,18 +7,6 @@ module "aa" {
 
   scale-set-id = azurerm_windows_virtual_machine_scale_set.win-vm-ss.id
 
-  tags = var.tags
-}
-
-module "monitor" {
-  source = "../azure-monitor"
-
-  environment = var.environment
-  location    = var.location
-  service     = var.service
-
-  scale-set-id = azurerm_windows_virtual_machine_scale_set.win-vm-ss.id
-
 
   tags = var.tags
 }
