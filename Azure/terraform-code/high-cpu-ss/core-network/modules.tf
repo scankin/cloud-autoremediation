bcbff05cd2ec56698610ad22211dc516cea1b1bc
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-15 16:47:56 +0000

    removing modules call

--- Azure/terraform-code/high-cpu-ss/core-network/modules.tf
+++ Azure/terraform-code/high-cpu-ss/core-network/modules.tf
@@ -10,19 +10,6 @@ module "aa" {
   tags = var.tags
 }
 
-module "monitor" {
-  source = "../azure-monitor"
-
-  environment = var.environment
-  location    = var.location
-  service     = var.service
-
-  scale-set-id = azurerm_windows_virtual_machine_scale_set.win-vm-ss.id
-
-
-  tags = var.tags
-}
-
 module "asp" {
   source = "../app-service-plan"
 
