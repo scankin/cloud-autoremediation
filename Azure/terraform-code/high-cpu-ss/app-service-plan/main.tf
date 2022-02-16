a8a2ad12fff984e148e160aea675ccec510a1286
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-16 16:34:10 +0000

    Adding Azure Backend to Providers

--- Azure/terraform-code/high-cpu-ss/app-service-plan/main.tf
+++ Azure/terraform-code/high-cpu-ss/app-service-plan/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>2.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "tfstate-autoremediation"
+    storage_account_name = "value"
+    container_name = "value"
+    key = "dev-asp.tfstate"
+  }
 }
 
 provider "azurerm" {
--- Azure/terraform-code/high-cpu-ss/automation-account/main.tf
+++ Azure/terraform-code/high-cpu-ss/automation-account/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>2.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name = "tfstate-autoremediation"
+    storage_account_name = "tfstateautoremediation"
+    container_name = "tfstate"
+    key = "dev-aa.tfstate"
+  }
 }
 
 provider "azurerm" {
