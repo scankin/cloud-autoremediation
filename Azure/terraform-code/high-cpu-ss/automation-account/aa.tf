054822e6b9b6be82c6fb7629fc608f74b85e0d1d
SCANKIN <scott.ankin@devopsgroup.com>
2022-03-01 16:32:23 +0000

    Fixing typo

--- Azure/terraform-code/high-cpu-ss/automation-account/aa.tf
+++ Azure/terraform-code/high-cpu-ss/automation-account/aa.tf
@@ -69,7 +69,7 @@ resource "azurerm_automation_webhook" "alert-webhook" {
 #######################################################################################
 
 resource "azurerm_automation_runbook" "restart-service" {
-  name = "Restart Service"
+  name = "Restart-Service"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   automation_account_name = azurerm_automation_account.aa.name
